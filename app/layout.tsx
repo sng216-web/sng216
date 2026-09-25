1ca6a8b66a6c1df9f0344edd6e83c2aa1499aaa2
sng216 <sng216@rambler.ru>
2026-09-25 21:02:05 +0700

    Add editorial site shell

---
+++ app/layout.tsx
@@ -0,0 +1,1 @@
+import "./globals.css"; export const metadata={title:"Редакция | Публикационный контур"}; export default function RootLayout({children}:{children:React.ReactNode}){return <html lang="ru"><body>{children}</body></html>}
